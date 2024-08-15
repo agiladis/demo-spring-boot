0df4a7ba02173cb7c5b66bc4e38c29ef3a3c7601
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-08-15 23:51:58 +0700

    [ADD] Todo model class

---
+++ src/main/java/com/example/demospringboot/model/Todo.java
@@ -0,0 +1,29 @@
+package com.example.demospringboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "spring-boot-todoapp")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed;
+}
