8a66c5033122b621c381161fe52c41049b347240
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-08-16 11:40:45 +0700

    [ADD] complete CRUD service for todo app

---
+++ src/main/java/com/example/demospringboot/controller/TodoController.java
@@ -0,0 +1,54 @@
+package com.example.demospringboot.controller;
+
+import com.example.demospringboot.model.Todo;
+import com.example.demospringboot.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+        Todo todo = todoService.getTodoById(id);
+
+        return ResponseEntity.ok(todo);
+    }
+
+    @PostMapping
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+        Todo createdTodo = todoService.createTodo(todo);
+
+        return ResponseEntity.ok(createdTodo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        Todo updatedTodo = todoService.updateTodo(id, todo);
+
+        return ResponseEntity.ok(updatedTodo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+
+        return ResponseEntity.ok().build();
+    }
+}
